7d81e6415f9eb9a284c2f8fa33623833bded4e24
bitcamp <bitcamp@DESKTOP-GBUVHE8>
2021-07-28 20:31:29 +0900

    first commit

---
+++ src/ch01_3_java_process/Hello.java
@@ -0,0 +1,10 @@
+package ch01_3_java_process;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello~~~");
+	}
+}
+	
+
+
---
+++ src/ch01_3_java_process/Hello.java.java
@@ -0,0 +1,7 @@
+package ch01_3_java_process;
+
+//public class Hello {
+//	public static void main(String[] args) {
+//	
+//	} 
+//}
